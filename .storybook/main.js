491c1f3253039a2123759c9c67865ec992679778
James Njoroge <mwithigajames@outlook.com>
2022-04-13 11:22:44 +0300

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  /** Expose public folder to storybook as static */
+  "staticDirs": ['../public'],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
