7ca978bbf2b3e6b3ff5acaa56c90d91cd535d7ad
James Njoroge <mwithigajames@outlook.com>
2022-04-13 12:24:03 +0300

    build: created base template component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,9 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'It is a beautiful beginning!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
